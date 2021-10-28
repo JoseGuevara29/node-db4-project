22674b2462f262512946fbb4eea2b29df8fa9d79
joseguevara29 <guevarabus29@gmail.com>
2021-10-27 21:06:20 -0400

    wip

--- data/migrations/20211026022106_initial-migration.js
+++ data/migrations/20211026022106_initial-migration.js
@@ -5,33 +5,40 @@ exports.up = async function (knex) {
       table.string("recipe_name", 200).notNullable().unique();
     })
     .createTable("ingredients", (table) => {
-      table.increments("ingredients_id");
-      table.string("ingredients_name", 200).notNullable().unique();
-      table.string("ingredients_unit", 50);
+      table.increments("ingredient_id");
+      table.string("ingredient_name", 200).notNullable().unique();
+      table.string("ingredient_unit", 50);
     })
     .createTable("steps", (table) => {
       table.increments("step_id");
       table.string("step_text", 200).notNullable();
-      table.integer("stemp_number").notNullable();
+      table.integer("step_number").notNullable();
       table
         .integer("recipe_id")
         .unsigned()
         .notNullable()
-        .refrences("recipe_id")
+        .references("recipe_id")
         .inTable("recipes")
         .onDelete("RESTRICT")
         .onUpdate("RESTRICT");
     })
     .createTable("step_ingredients", (table) => {
-      table.increments("step_id");
-      table.string("step_text", 200).notNullable();
-      table.integer("stemp_number").notNullable();
+      table.increments("step_ingredient_id");
+      table.float("quantity").notNullable();
       table
-        .integer("recipe_id")
+        .integer("step_id")
         .unsigned()
         .notNullable()
-        .refrences("recipe_id")
+        .references("step_id")
         .inTable("recipes")
+        .onDelete("RESTRICT")
+        .onUpdate("RESTRICT");
+      table
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredient_id")
+        .inTable("ingredients")
         .onDelete("RESTRICT")
         .onUpdate("RESTRICT");
     });
